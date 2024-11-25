17e574f70e92f785f4b507e5ba60ea45d3b375c7
Shubham Nagar <38903550+shubham184@users.noreply.github.com>
2024-11-25 21:21:18 +0100

    Create example_pyspark_dag.py

---
+++ example_pyspark_dag.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime
+from pyspark.sql import SparkSession
+
+# Define the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+}
+with DAG(
+    dag_id="example_pyspark_dag",
+    default_args=default_args,
+    description="Simple DAG using PySpark",
+    schedule_interval=None,  # Run manually
+    start_date=datetime(2023, 11, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    @task.pyspark(conn_id="spark-local")  # Use Spark locally
+    def spark_task(spark: SparkSession) -> None:
+        # Create a simple DataFrame
+        df = spark.createDataFrame(
+            [
+                (1, "John Doe", 21),
+                (2, "Jane Doe", 22),
+                (3, "Joe Bloggs", 23),
+            ],
+            ["id", "name", "age"],
+        )
+        # Show DataFrame content in logs
+        df.show()
+
+    # Define task in the DAG
+    task1 = spark_task()
