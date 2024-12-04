c9ce7c0f019452d8c60f4362114029d2a7fa9c1d
Shubham <mail.shubhamnagar@gmail.com>
2024-12-04 16:10:17 +0530

    Implement transform method in transformers to convert input data into DataFrames

--- src/transformers/orderline_steps_transformer.py
+++ src/transformers/orderline_steps_transformer.py
@@ -1,5 +1,14 @@
 from models.base_table_transformer import BaseTableTransformer
+from pyspark.sql import DataFrame
+from typing import Tuple, List
 
 class OrderlineStepsTransformer(BaseTableTransformer):
     def __init__(self, spark, schema_config):
         super().__init__(spark, schema_config, 'channel1_orderline-steps')
+
+    def transform(self, data) -> List[Tuple[str, DataFrame]]:
+        """
+        Transform the input data into DataFrames.
+        This method wraps transform_docs_to_dataframes to satisfy the abstract method requirement.
+        """
+        return self.transform_docs_to_dataframes(data)
--- src/transformers/user_transformer.py
+++ src/transformers/user_transformer.py
@@ -1,5 +1,13 @@
 from models.base_table_transformer import BaseTableTransformer
+from pyspark.sql import DataFrame
+from typing import Tuple, List
 
 class UserTransformer(BaseTableTransformer):
     def __init__(self, spark, schema_config):
         super().__init__(spark, schema_config, 'channel1_user')
+    def transform(self, data) -> List[Tuple[str, DataFrame]]:
+        """
+        Transform the input data into DataFrames.
+        This method wraps transform_docs_to_dataframes to satisfy the abstract method requirement.
+        """
+        return self.transform_docs_to_dataframes(data)
